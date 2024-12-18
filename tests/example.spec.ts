b828123be8adf729bac34f160b78da6e1511f9d9
Donny Gumelar France <Donny_F7234@mitrais.com>
2024-12-18 11:29:46 +0800

    First commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Button', async ({ page }) => {
+  await page.goto('https://cmsautomation.mitrais.com/CRS.UI.Web/Login/LogOn');
+
+  //Login
+  await page.waitForSelector("[id='btn-login']", { state: 'visible' }); //Login page
+  await page.click("[id='btn-login']"); //Click Login button
+  await expect(page).toHaveTitle(/Sign in/);
+  await page.waitForSelector("[id='i0116']", { state: 'visible' }); //Email field
+  await page.fill('input[id="i0116"]', 'rommy@periodictabledev.onmicrosoft.com'); //email field input
+  await page.waitForSelector("[id='idSIButton9']", { state: 'visible' }); //button next
+  await page.click("[id='idSIButton9']"); //click button next
+  await page.waitForSelector("[id='i0118']", { state: 'visible' }); //password field
+  await page.fill('input[id="i0118"]', 'Ngg4kt4u'); //password field input
+  await page.waitForSelector("[id='idSIButton9']", { state: 'visible' }); //button submit
+  await page.click("[id='idSIButton9']"); //click button submit
+
+  //Homepage
+  await page.waitForSelector("[id='EmployeeAlias']", { state: 'visible' }); //Homepage welcome
+
+   //Usermenu drop-down
+   await page.waitForSelector("[id='newEmpRole']", { state: 'visible' }); //User menu drop-down
+   await page.click("[id='newEmpRole']"); //click drop-down
+ 
+   //logout-btn
+   await page.waitForSelector("[id='logout-btn']", { state: 'visible' }); //User menu drop-down
+   await page.click("[id='logout-btn']"); //click drop-down
+   await page.waitForSelector("[id='btn-login']", { state: 'visible' }); //Login page
+   
+});
+
+// test('LogOut', async ({ page }) => {
+ 
+
+// });
